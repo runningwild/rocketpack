8ee1d4d48ea68af1e35600c1bcae5f7c801f1de0
Jonathan Wills <runningwild@gmail.com>
2015-11-16 14:00:18 -0500

    c -> ctx for real this time

--- app/app.go
+++ app/app.go
@@ -15,18 +15,18 @@ import (
 	"appengine/datastore"
 )
 
-func serveError(c appengine.Context, w http.ResponseWriter, err error) {
+func serveError(ctx appengine.Context, w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusInternalServerError)
 	w.Header().Set("Content-Type", "text/plain")
 	io.WriteString(w, "Internal Server Error")
-	c.Errorf("%v", err)
+	ctx.Errorf("%v", err)
 }
 
 func handlePrepareUpload(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
-	uploadURL, err := blobstore.UploadURL(c, "/upload", nil)
+	uploadURL, err := blobstore.UploadURL(ctx, "/upload", nil)
 	if err != nil {
-		serveError(c, w, err)
+		serveError(ctx, w, err)
 		return
 	}
 	w.Header().Set("Content-Type", "text/html")
@@ -51,36 +51,36 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 	// Sanity check all of the inputs.
 	blobs, other, err := blobstore.ParseUpload(r)
 	if err != nil {
-		serveError(c, w, err)
+		serveError(ctx, w, err)
 		return
 	}
 	if len(other["name"]) != 1 || len(other["version"]) != 1 || len(other["os"]) != 1 || len(other["arch"]) != 1 || len(other["signature"]) != 1 {
-		serveError(c, w, fmt.Errorf("must specify all of name, version, os, arch and signature"))
+		serveError(ctx, w, fmt.Errorf("must specify all of name, version, os, arch and signature"))
 		return
 	}
 	if len(blobs) != 1 || len(blobs["file"]) != 1 {
 		log.Printf("Got blobs: %v", blobs)
 		// log.Printf("Got others: %v", other)
-		serveError(c, w, fmt.Errorf("must specify exactly one upload file named 'file' as the container to upload"))
+		serveError(ctx, w, fmt.Errorf("must specify exactly one upload file named 'file' as the container to upload"))
 		return
 	}
 	signature, err := ioutil.ReadAll(base64.NewDecoder(base64.URLEncoding, bytes.NewBuffer([]byte(other["signature"][0]))))
 	if err != nil {
-		serveError(c, w, fmt.Errorf("got an error while trying to read signature: %v", err))
+		serveError(ctx, w, fmt.Errorf("got an error while trying to read signature: %v", err))
 		return
 	}
 
 	// Check for an existing registry, create it if necessary.
-	rkey := registryKey(c, other["name"][0])
+	rkey := registryKey(ctx, other["name"][0])
 	var registry ContainerRegistry
-	err = datastore.Get(c, rkey, &registry)
+	err = datastore.Get(ctx, rkey, &registry)
 	if err != nil && err != datastore.ErrNoSuchEntity {
-		serveError(c, w, fmt.Errorf("unable to find registry: %v", err))
+		serveError(ctx, w, fmt.Errorf("unable to find registry: %v", err))
 		return
 	}
 	if err == datastore.ErrNoSuchEntity {
-		if _, err := datastore.Put(c, rkey, &ContainerRegistry{Name: other["name"][0]}); err != nil {
-			serveError(c, w, fmt.Errorf("failed to create container registry: %v", err))
+		if _, err := datastore.Put(ctx, rkey, &ContainerRegistry{Name: other["name"][0]}); err != nil {
+			serveError(ctx, w, fmt.Errorf("failed to create container registry: %v", err))
 		}
 	}
 
@@ -94,21 +94,21 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 		Filter("Version =", other["version"][0]).
 		Filter("OS =", other["os"][0]).
 		Filter("Arch =", other["arch"][0]).
-		GetAll(c, &existing)
+		GetAll(ctx, &existing)
 	if err != nil {
-		serveError(c, w, fmt.Errorf("unable to query registry: %v", err))
+		serveError(ctx, w, fmt.Errorf("unable to query registry: %v", err))
 	}
 	for i := range existing {
-		if err := blobstore.Delete(c, existing[i].BlobKey); err != nil {
-			serveError(c, w, fmt.Errorf("unable to delete existing container: %v", err))
+		if err := blobstore.Delete(ctx, existing[i].BlobKey); err != nil {
+			serveError(ctx, w, fmt.Errorf("unable to delete existing container: %v", err))
 		}
-		if err := datastore.Delete(c, keys[i]); err != nil {
-			serveError(c, w, fmt.Errorf("unable to remove existing container: %v", err))
+		if err := datastore.Delete(ctx, keys[i]); err != nil {
+			serveError(ctx, w, fmt.Errorf("unable to remove existing container: %v", err))
 		}
 	}
 
 	// Put the container into the datastore
-	key := datastore.NewIncompleteKey(c, "Container", rkey)
+	key := datastore.NewIncompleteKey(ctx, "Container", rkey)
 	entry := &Container{
 		Version:   other["version"][0],
 		Arch:      other["arch"][0],
@@ -116,8 +116,8 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 		Signature: string(signature),
 		BlobKey:   blobs["file"][0].BlobKey,
 	}
-	if _, err := datastore.Put(c, key, entry); err != nil {
-		serveError(c, w, err)
+	if _, err := datastore.Put(ctx, key, entry); err != nil {
+		serveError(ctx, w, err)
 		return
 	}
 }
@@ -126,8 +126,8 @@ type ContainerRegistry struct {
 	Name string
 }
 
-func registryKey(c appengine.Context, name string) *datastore.Key {
-	key := datastore.NewKey(c, "ContainerRegistry", name, 0, nil)
+func registryKey(ctx appengine.Context, name string) *datastore.Key {
+	key := datastore.NewKey(ctx, "ContainerRegistry", name, 0, nil)
 	if key == nil {
 		log.Printf("Didn't get a key from datastore.NewKey()")
 	}
@@ -145,7 +145,7 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 
 	parts := matchAci.FindStringSubmatch(r.URL.String())
 	if len(parts) < 7 {
-		serveError(c, w, fmt.Errorf("invalid request"))
+		serveError(ctx, w, fmt.Errorf("invalid request"))
 		return
 	}
 	name := parts[2]
@@ -155,12 +155,12 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	asc := parts[6] == ".asc"
 
 	var registry ContainerRegistry
-	rkey := registryKey(c, name)
-	if err := datastore.Get(c, rkey, &registry); err != nil {
-		serveError(c, w, fmt.Errorf("unable to get reigstry %q: %v", name, err))
+	rkey := registryKey(ctx, name)
+	if err := datastore.Get(ctx, rkey, &registry); err != nil {
+		serveError(ctx, w, fmt.Errorf("unable to get reigstry %q: %v", name, err))
 		return
 	}
-	it := datastore.NewQuery("Container").Ancestor(rkey).Run(c)
+	it := datastore.NewQuery("Container").Ancestor(rkey).Run(ctx)
 	var newest string
 	var signature string
 	var bkey appengine.BlobKey
@@ -172,7 +172,7 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 		if err != nil {
-			serveError(c, w, err)
+			serveError(ctx, w, err)
 			return
 		}
 		if c.OS != "" && c.OS != os {
@@ -197,7 +197,7 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 	if !found {
-		serveError(c, w, fmt.Errorf("failed to find container"))
+		serveError(ctx, w, fmt.Errorf("failed to find container"))
 		return
 	}
 	if asc {
