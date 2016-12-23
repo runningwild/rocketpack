be0cb07bf82889fb34ddd298af7030695bbc5592
Jonathan Wills <runningwild@gmail.com>
2016-12-23 11:49:02 -0500

    fixed up the proxy

--- proxy/main.go
+++ proxy/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"net/url"
 	"strings"
 	"time"
 
@@ -15,7 +19,7 @@ import (
 var (
 	domain      = flag.String("domain", "", "Domain to serve containers from.")
 	port        = flag.Int("port", 443, "Port to serve https traffic on.")
-	dst         = flag.String("dst", "localhost:8080", "Address to forward traffic to.")
+	dst         = flag.String("dst", "http://localhost:8081", "Address to forward traffic to.")
 	tlsCertFile = flag.String("cert-file", "", "PEM encoded certificates.")
 	tlsKeyFile  = flag.String("key-file", "", "PEM encoded key.")
 )
@@ -25,7 +29,7 @@ func main() {
 
 	go func() {
 		for {
-			time.Sleep(time.Second * 5)
+			time.Sleep(time.Second * 25)
 			_, err := http.Get("http://www.google.com")
 			if err != nil {
 				log.Printf("failed to find google: %v", err)
@@ -45,10 +49,72 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
-	r.PathPrefix("").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		data, _ := json.MarshalIndent(req, "", "  ")
-		fmt.Printf("%s\n", data)
+	dstUrl, err := url.Parse(*dst)
+	if err != nil {
+		log.Fatalf("Failed to parse %q as a valid URL: %v", *dst, err)
+	}
+	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		fmt.Printf("Req: %s\n", reqToStr(req))
+		if *dst == "" {
+			return
+		}
+		req.Host = ""
+		req.RequestURI = ""
+		req.URL = dstUrl
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Printf("Failed proxy request: %v\n", err)
+			return
+		}
+		io.Copy(w, resp.Body)
 	})
 
 	log.Fatalf("%v", http.ListenAndServeTLS(fmt.Sprintf(":%d", *port), *tlsCertFile, *tlsKeyFile, r))
+}
+
+func reqToStr(req *http.Request) string {
+	data, _ := json.MarshalIndent(&Request{
+		Method:           req.Method,
+		URL:              req.URL,
+		Proto:            req.Proto,
+		ProtoMajor:       req.ProtoMajor,
+		ProtoMinor:       req.ProtoMinor,
+		Header:           req.Header,
+		Body:             req.Body,
+		ContentLength:    req.ContentLength,
+		TransferEncoding: req.TransferEncoding,
+		Close:            req.Close,
+		Host:             req.Host,
+		Form:             req.Form,
+		PostForm:         req.PostForm,
+		MultipartForm:    req.MultipartForm,
+		Trailer:          req.Trailer,
+		RemoteAddr:       req.RemoteAddr,
+		RequestURI:       req.RequestURI,
+		TLS:              req.TLS,
+		Response:         req.Response,
+	}, "", "  ")
+	return string(data)
+}
+
+type Request struct {
+	Method           string
+	URL              *url.URL
+	Proto            string // "HTTP/1.0"
+	ProtoMajor       int    // 1
+	ProtoMinor       int    // 0
+	Header           http.Header
+	Body             io.ReadCloser
+	ContentLength    int64
+	TransferEncoding []string
+	Close            bool
+	Host             string
+	Form             url.Values
+	PostForm         url.Values
+	MultipartForm    *multipart.Form
+	Trailer          http.Header
+	RemoteAddr       string
+	RequestURI       string
+	TLS              *tls.ConnectionState
+	Response         *http.Response
 }
